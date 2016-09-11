d453808e6c85af25d8efa1f009040e4a8a8bee2d
Will Price <will.price94@gmail.com>
2016-09-12 00:05:55 +0100

    Update new.sh to create correctly named predicates

--- new.sh
+++ new.sh
@@ -4,23 +4,30 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-NAME="$1"; shift
+FN_NAME="$1"; shift
 
-SOURCE="${NAME}.rkt"
-TEST="tests/${NAME}-test.rkt"
+if [[ $FN_NAME == *"?" ]]; then
+    BASE_NAME="${FN_NAME%%?}-p"
+else
+    BASE_NAME="${FN_NAME}"
+fi
 
-for file in "${SOURCE}" "${TEST}"; do
+SOURCE_PATH="${BASE_NAME}.rkt"
+TEST_PATH="tests/${BASE_NAME}-test.rkt"
+
+for file in "${SOURCE_PATH}" "${TEST_PATH}"; do
     if [ -f "$file" ]; then
         echo "$file already exists, quitting"
         exit 1
     fi
 done
 
-echo "creating $NAME files"
+echo "creating $FN_NAME files"
 
-cp base.rkt.tmpl "${SOURCE}"
-cp base-test.rkt.tmpl "${TEST}"
+cp base.rkt.tmpl "${SOURCE_PATH}"
+cp base-test.rkt.tmpl "${TEST_PATH}"
 
-for file in "${SOURCE}" "${TEST}"; do
-    sed -i "s/NAME/${NAME}/" "$file"
+for file in "${SOURCE_PATH}" "${TEST_PATH}"; do
+    sed -i "s/FN_NAME/${FN_NAME}/" "$file"
+    sed -i "s/BASE_NAME/${BASE_NAME}/" "$file"
 done
