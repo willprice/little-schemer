5d7b373c8a510d09a70b43da86d69ecdbf7a1df5
Will Price <will.price94@gmail.com>
2016-09-17 18:39:26 +0100

    Refactor: Move my-equal to `equality.rkt`

--- tests/equality.rkt
+++ tests/equality.rkt
@@ -11,3 +11,22 @@
   (check-equal? (eqan? 1 2) #f)
   (check-equal? (eqan? 1 'a) #f)
   (check-equal? (eqan? 'a 1) #f))
+
+(test-case
+  "eqlist?"
+  (check-equal? (eqlist? '() '()) #t)
+  (check-equal? (eqlist? '(a) '()) #f)
+  (check-equal? (eqlist? '() '(a)) #f)
+  (check-equal? (eqlist? '(a) '(a)) #t)
+  (check-equal? (eqlist? '(b) '(a)) #f)
+  (check-equal? (eqlist? '(a b) '(a)) #f)
+  (check-equal? (eqlist? '(a b) '(a b)) #t)
+  (check-equal? (eqlist? '(a b) '(a a)) #f))
+
+(test-case
+  "equal?"
+  (check-equal? (my-equal? '() '()) #t)
+  (check-equal? (my-equal? '() 'a) #f)
+  (check-equal? (my-equal? 'a 'a) #t)
+  (check-equal? (my-equal? 1 1) #t)
+  (check-equal? (my-equal? '(a) '(a)) #t))
