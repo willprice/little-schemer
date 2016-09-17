7ed2ca90a47a391542e644b73c5aa5b6f62e7e98
Will Price <will.price94@gmail.com>
2016-09-17 11:52:22 +0100

    Cleanup: Remove one.rkt and associated tests

--- one.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket/base
-
-(define one?
-  (lambda (n)
-    (eq? n 1)))
-
-(provide one?)
--- tests/one-test.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket/base
-
-(require rackunit
-         "../one.rkt")
-
-(check-equal? (one? 0) #f)
-(check-equal? (one? 1) #t)
