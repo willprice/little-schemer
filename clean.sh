e1c442b9c93c74c88820b558c795bb62bfa92a6b
Will Price <will.price94@gmail.com>
2016-09-17 18:37:28 +0100

    Bugfix: Clean recursively rather than just top level

--- clean.sh
+++ clean.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+shopt -s globstar # (enable ** matching)
+
 rm -r compiled \
-      *.rkt~ \
-      \#*.rkt\#*\# \
+      **/*.rkt~ \
+      **/\#*.rkt\#*\# \
+      **/coverage \
       &> /dev/null
 exit 0
